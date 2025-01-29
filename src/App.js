674a2ce6514c9387224afffaf795504e6f5f582a
mlumanta <lumanta.mikhail21299@gmail.com>
2025-01-29 17:47:46 +0800

    API URL update

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ const App = () => {
   const fetchAllPeople = async () => {
     setLoading(true)
     let allPeople = []
-    let page = "https://swapi.py4e.com/api/people/"
+    let page = "https://swapi.dev/api/people/"
 
     try {
       while(page) {
@@ -106,7 +106,7 @@ const App = () => {
   const fetchAllPlanets = async () => {
     setLoading(true)
     let allPlanets = []
-    let page = "https://swapi.py4e.com/api/planets/"
+    let page = "https://swapi.dev/api/planets/"
 
     try {
       while(page) {
